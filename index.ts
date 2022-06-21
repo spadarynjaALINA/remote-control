e49fe97ec78092c2685c5648720b49503ea408df
spadarynjaALINA <alinagusarik@gmail.com>
2022-06-21 21:34:13 +0300

    feat:add server and klient

--- index.ts
+++ index.ts
@@ -1,27 +1,34 @@
 import Jimp from 'jimp';
 import {httpServer} from './src/http_server/index';
 import robot from 'robotjs';
-import WebSocket, { WebSocketServer } from 'ws';
+import  { WebSocketServer } from 'ws';
 
 const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen( HTTP_PORT );
-const wss = new WebSocket( 'ws:localhost:3000' )
-wss.onmessage = ( message ) =>
-{console.log(message)
-
-}
-
+const wss = new WebSocketServer( { port: 8080 } )
 wss.on( 'connection', ws =>
 {
-	ws.on( 'message',( data:string) =>
+	ws.on( 'message', ( data: string ) =>
 	{
-		console.log('data',data)
+		console.log( 'data', data )
+	})
+	ws.send( JSON.stringify( {
+		type: "hello from server",
+		content: [1, "2"]
 	} )
-	ws.send('som')
-} )
+	)
+})
+
 wss.on( 'close', () =>
 {
 	console.log('close wss')
+})
+document.addEventListener( 'keydown', ( e ) =>
+{
+	if ( e.code === '' )
+	{
+
+	}
 })
--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
-
+import WebSocket from 'ws';
 export const httpServer = http.createServer(function (req, res) {
     const __dirname = path.resolve(path.dirname(''));
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
@@ -14,4 +14,17 @@ export const httpServer = http.createServer(function (req, res) {
         res.writeHead(200);
         res.end(data);
     });
+} );
+const socket = new WebSocket("ws://localhost:8080");
+socket.addEventListener("open", () => {
+  // send a message to the server
+  socket.send(JSON.stringify({
+    type: "hello from client",
+    content: [ 3, "4" ]
+  }));
+});
+
+// receive a message from the server
+socket.addEventListener("message", ({ data }) => {
+ console.log(data)
 });
