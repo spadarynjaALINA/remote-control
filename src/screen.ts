c77b93834b36d781d8894d1f4249246a34fee75b
spadarynjaALINA <alinagusarik@gmail.com>
2022-06-25 10:00:11 +0300

    fix:fix color problems in the screen func

--- src/screen.ts
+++ src/screen.ts
@@ -5,17 +5,27 @@ import WebSocket from 'ws';
 export const screen = async ( width: number, height: number, ws: WebSocket.WebSocket ) =>
 {
 		const mouse = robot.getMousePos()
-const x = mouse.x - width
-const y = mouse.y - height
-		console.log(x,y,x-width,y-height)
-	const bitMap = robot.screen.capture( x, y, width*2, height*2 );
-	console.log(x,y)
-	const img = new Jimp( width * 2, height * 2 );
-	var multi = bitMap.width /  width * 2;
-var color =bitMap.colorAt(2 * multi, 3 * multi);
-
-  img.bitmap.data = bitMap.image;
-const base64= await img.getBufferAsync( Jimp.MIME_PNG)
+ const x = mouse.x - width
+ const y = mouse.y - height
+	const pic = robot.screen.capture( x, y, width * 2, height * 2 );
+	const w = pic.byteWidth / pic.bytesPerPixel
+  const h = pic.height
+	const image = new Jimp( w, h );
+	let red:number, green:number,blue:number
+  pic.image.forEach((byte:number, i:number) => {
+    switch (i % 4) {
+      case 0: return blue = byte
+      case 1: return green = byte
+      case 2: return red = byte
+      case 3:
+        image.bitmap.data[i - 3] = red
+        image.bitmap.data[i - 2] = green
+        image.bitmap.data[i - 1] = blue
+        image.bitmap.data[i] = 255
+    }
+  })
+  // image.bitmap.data = pic.image;
+const base64= await image.getBufferAsync( Jimp.MIME_PNG)
  ws.send(`prnt_scrn ${base64.toString('base64')}\0`);
 
 	}
